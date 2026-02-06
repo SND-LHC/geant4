ec10e1089cc8f35d4a334daf40617859670238f9
siilieva <simona.ilieva.ilieva@cern.ch>
2026-02-06 19:50:56 +0100

    pff

--- source/run/src/G4VUserPhysicsList.cc
+++ source/run/src/G4VUserPhysicsList.cc
@@ -87,7 +87,7 @@ G4VUserPhysicsList::G4VUserPhysicsList()
 {
   g4vuplInstanceID = subInstanceManager.CreateSubInstance();  // AND
   // default cut value  (1.0mm)
-  defaultCutValue = 0.1 * mm;
+  defaultCutValue = 100 * um;
 
   // pointer to the particle table
   theParticleTable = G4ParticleTable::GetParticleTable();
